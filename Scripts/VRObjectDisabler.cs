20cc3c479a5f393093e131fe939cab3ea31b2b0c
Linya Ruan <133128176+RuanLinya@users.noreply.github.com>
2023-07-20 12:01:22 +0200

    Add files via upload

---
+++ Scripts/VRObjectDisabler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class VRObjectDisabler : MonoBehaviour
+{
+    public XRController controller;
+
+    private XRInteractorLineVisual lineVisual;
+    private LineRenderer lineRenderer;
+
+    private void Start()
+    {
+        lineVisual = controller.GetComponent<XRInteractorLineVisual>();
+      
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(lineRenderer.transform.position, lineRenderer.transform.forward, out hit))
+        {
+            // 检测到碰撞，将碰撞到的物体设为不活跃状态
+            hit.collider.gameObject.SetActive(false);
+        }
+    }
+}
